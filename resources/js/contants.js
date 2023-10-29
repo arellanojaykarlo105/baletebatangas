aed215f8f313936f080cca33214bf2a4ffdf61bd
Chester Agsamosam <chester.agsamosam@collabera.com>
2023-10-29 18:11:15 +0800

    update constats.js

--- resources/js/contants.js
+++ resources/js/contants.js
@@ -1,19 +1,17 @@
-export const barangays = [
-  {
-    alangilan: {
-      name: 'Alangilan',
-      description: 'Ang lumang bayan ay blahbalahl balha',
-      img: 'https://'
-    },
-    sanPedro: {
-      name: 'San Pedro',
-      description: 'Ang lumang bayan ay blahbalahl balha',
-      img: '/images/backgroundimg.jpeg'
-    },
-    sanPedro: {
-      name: 'San Pedro',
-      description: 'Ang lumang bayan ay blahbalahl balha',
-      img: '/images/backgroundimg.jpeg'
-    }
+export const barangays = {
+  alangilan: {
+    name: 'Alangilan',
+    description: 'Ang lumang bayan ay blahbalahl balha',
+    img: 'https://'
+  },
+  sanPedro: {
+    name: 'San Pedro',
+    description: 'Ang lumang bayan ay blahbalahl balha',
+    img: '/images/backgroundimg.jpeg'
+  },
+  sanPedro: {
+    name: 'San Pedro',
+    description: 'Ang lumang bayan ay blahbalahl balha',
+    img: '/images/backgroundimg.jpeg'
+  }
 }
-]
