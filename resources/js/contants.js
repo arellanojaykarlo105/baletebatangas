83000d8033b7051ce0d1edbf33963e624717f50a
Chester Agsamosam <chester.agsamosam@collabera.com>
2023-10-29 18:07:13 +0800

    add contants

---
+++ resources/js/contants.js
@@ -0,0 +1,19 @@
+export const barangays = [
+  {
+    alangilan: {
+      name: 'Alangilan',
+      description: 'Ang lumang bayan ay blahbalahl balha',
+      img: 'https://'
+    },
+    sanPedro: {
+      name: 'San Pedro',
+      description: 'Ang lumang bayan ay blahbalahl balha',
+      img: '/images/backgroundimg.jpeg'
+    },
+    sanPedro: {
+      name: 'San Pedro',
+      description: 'Ang lumang bayan ay blahbalahl balha',
+      img: '/images/backgroundimg.jpeg'
+    }
+}
+]
